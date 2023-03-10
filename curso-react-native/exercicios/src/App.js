f8ec4f9da500ce7a14e8bd09b97ea61f9b918e25
ViniciusSP1995 <vinicius.silva@trendsis.com.br>
2023-03-09 21:22:46 -0300

    desenvolvimento primeiro componente

---
+++ curso-react-native/exercicios/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import  { Text, View } from 'react-native';
+
+import Primeiro from './components/Primeiro'
+
+export default () => (
+    <View>
+        <Text>{ 1 + 1 }</Text>
+        <Primeiro/>
+    </View>
+
+    )
+    
+
